a626a0d2d862fce9de98a1cd9b85b58f5d8d2afe
Bai Web and Mobile Lab <irayspacii@gmail.com>
2019-12-27 19:26:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## F9T9IT
+
+9T9IT Setup App for ERPNext
+
+#### License
+
+MIT
---
+++ f9t9it/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "F9T9IT",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("F9T9IT")
+		}
+	]
