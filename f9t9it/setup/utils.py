4cbfee0b7d66e44fd0abcd925f8d523593b639f8
Ivan Ray Altomera <irayspacii@gmail.com>
2020-01-03 00:46:20 +0800

    feat: disable doctype functions

--- f9t9it/setup/utils.py
+++ f9t9it/setup/utils.py
@@ -17,3 +17,22 @@ def set_readonly_property_setter(doctype, fieldname):
         'property': 'read_only',
         'value': '1'
     })
+
+
+def disable_doctype(doctype):
+    """
+    If Custom DocPerm is not setup (not transferred to cache), this will work.
+    :param doctype: DocType to disable
+    :return:
+    """
+    custom_docperm = frappe.get_doc({
+        'doctype': 'Custom DocPerm',
+        'parent': doctype,
+        'parenttype': 'DocType',
+        'parentfield': 'permissions',
+        'role': 'System Manager',
+        'permlevel': 0,
+        'read': 0,
+        'export': 0
+    })
+    custom_docperm.save()
